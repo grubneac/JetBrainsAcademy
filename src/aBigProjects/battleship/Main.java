3a8fc14a8fbc7d7de427d49cc73f36ad60bcf991
Alexandru.Grubneac <grubneac@gmail.com>
2020-11-04 13:14:35 +0200

    In this stage, you should arrange your ships on the game field. Before you start, let's discuss the conventions of the game:
    1 On a 10x10 field, the first row should contain numbers from 1 to 10 indicating the column, and the first column should contain letters from A to J indicating the row.
    2 The symbol ~ denotes the fog of war: the unknown area on the opponent's field and the yet untouched area on your field.
    3 The symbol O denotes a cell with your ship, X denotes that the ship was hit, and M signifies a miss.
    4 You have 5 ships: Aircraft Carrier is 5 cells, Battleship is 4 cells, 5 Submarine is 3 cells, Cruiser is also 3 cells, and Destroyer is 2 cells. Start placing your ships with the largest one.
    6 To place a ship, enter two coordinates: the beginning and the end of the ship.
    If an error occurs in the input coordinates, your program should report it. The message should contain the word Error.

---
+++ src/aBigProjects/battleship/Main.java
@@ -0,0 +1,264 @@
+package aBigProjects.battleship;
+/*
+Battleship (also called Battleships or Sea Battle) is a two-player strategy game whose history traces back
+to the First World War. It started off as a pencil and paper game, until Milton Bradley coined the rules and published
+the game. Fun fact: it was one of the first games to be produced as a computer game in 1979! In this project, we will
+recreate this timeless classic.
+
+First off, brush up on the rules of the game(https://en.wikipedia.org/wiki/Battleship_(game)).
+There are different variations of the Battleship game, but we will
+stick to the original rules written by Milton Bradley. You have a 10×10 game field and five ships to arrange on
+that field. The ships can be placed horizontally or vertically but not diagonally across the grid spaces; the ships
+should not cross or touch each other. The goal is to sink all the ships of the opponent before your opponent
+does this to you.
+ */
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BattleField bf = new BattleField();
+        bf.print();
+        int curShip = 0;
+        String error = null;
+        while (curShip < 5) {
+            error = bf.putShipOnField(bf.getShip(curShip), error);
+
+            if (error == null) {
+                curShip++;
+                bf.print();
+            } else {
+                System.out.println(error);
+            }
+        }
+    }
+}
+
+class Ship {
+    private final String name;
+    private final int shipSize;
+    private Point begPos;
+    private Point endPos;
+
+    public Ship(String name, int shipSize) {
+        this.name = name;
+        this.shipSize = shipSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getShipSize() {
+        return shipSize;
+    }
+
+    public Point getBegPos() {
+        return begPos;
+    }
+
+    public void setBegPos(Point begPos) {
+        this.begPos = begPos;
+    }
+
+    public Point getEndPos() {
+        return endPos;
+    }
+
+    public void setEndPos(Point endPos) {
+        this.endPos = endPos;
+    }
+
+    @Override
+    public String toString() {
+        return "Ship{" +
+                "name='" + name + '\'' +
+                ", sizeCell=" + shipSize +
+                '}';
+    }
+}
+
+class Point {
+    private final int row;
+    private final int col;
+
+    public Point(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
+
+class BattleField {
+    private static final Scanner scanner = new Scanner(System.in);
+    private final int SIZE_FIELD = 10;
+    private final char START_LETTER = 'A';
+    private Ship[] ships;
+    private char[][] field;
+
+    public BattleField() {
+        initField();
+        initShips();
+    }
+
+    private void initField() {
+        field = new char[SIZE_FIELD][SIZE_FIELD];
+        for (int i = 0; i < SIZE_FIELD; i++) {
+            for (int j = 0; j < SIZE_FIELD; j++) {
+                field[i][j] = '~';
+            }
+        }
+    }
+
+    public void initShips() {
+        ships = new Ship[5];
+        ships[0] = new Ship("Aircraft Carrier", 5);
+        ships[1] = new Ship("Battleship", 4);
+        ships[2] = new Ship("Submarine", 3);
+        ships[3] = new Ship("Cruiser", 3);
+        ships[4] = new Ship("Destroyer", 2);
+    }
+
+    public void print() {
+        System.out.print(" ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE_FIELD; i++) {
+            System.out.print((char) (i + (int) START_LETTER));
+            for (int j = 0; j < SIZE_FIELD; j++) {
+                System.out.print(" " + field[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public Ship getShip(int ind) {
+        return ships[ind];
+    }
+
+    public String putShipOnField(Ship curShip, String error) {
+        if (error == null) {
+            System.out.printf("Enter the coordinates of the %s (%d cells):\n"
+                    , curShip.getName()
+                    , curShip.getShipSize()
+            );
+        }
+        System.out.println();
+        String[] shipPosition = scanner.nextLine().split("\\s+");
+        System.out.println();
+
+        curShip.setBegPos(new Point(convertToInt(shipPosition[0].substring(0, 1)), Integer.parseInt(shipPosition[0].substring(1)) - 1));
+        curShip.setEndPos(new Point(convertToInt(shipPosition[1].substring(0, 1)), Integer.parseInt(shipPosition[1].substring(1)) - 1));
+
+        int lenByRow = Math.abs(curShip.getEndPos().getRow() - curShip.getBegPos().getRow()) + 1;
+        int lenByCol = Math.abs(curShip.getEndPos().getCol() - curShip.getBegPos().getCol()) + 1;
+
+        if (curShip.getShipSize() != lenByRow && curShip.getShipSize() != lenByCol) { // ship len incorrect
+            return String.format("Error! Wrong length of the %s! Try again:", curShip.getName());
+        } else if (lenByCol != 1 && lenByRow != 1) { // try put ship by diagonal
+            return "Error! Wrong ship location! Try again:";
+        } //out of the field
+        if (curShip.getBegPos().getRow() >= SIZE_FIELD || curShip.getBegPos().getRow() < 0
+                || curShip.getBegPos().getCol() >= SIZE_FIELD || curShip.getBegPos().getCol() < 0
+                || curShip.getEndPos().getRow() >= SIZE_FIELD || curShip.getEndPos().getRow() < 0
+                || curShip.getEndPos().getCol() >= SIZE_FIELD || curShip.getEndPos().getCol() < 0
+        ) {
+            return "Error! Wrong ship location! Try again:";
+        } else if (isTooCloseToAnotherShip(curShip)) { //check too close to another ship
+            return "Error! You placed it too close to another one. Try again:";
+        } else {
+            installShip(curShip);
+            return null;
+        }
+    }
+
+    private void installShip(Ship curShip) {
+        int leftRow;
+        int leftCol;
+        int rightRow;
+        int rightCol;
+        if (curShip.getBegPos().getRow() > curShip.getEndPos().getRow()) {
+            rightRow = curShip.getBegPos().getRow();
+            leftRow = curShip.getEndPos().getRow();
+        } else {
+            leftRow = curShip.getBegPos().getRow();
+            rightRow = curShip.getEndPos().getRow();
+        }
+        if (curShip.getBegPos().getCol() > curShip.getEndPos().getCol()) {
+            rightCol = curShip.getBegPos().getCol();
+            leftCol = curShip.getEndPos().getCol();
+        } else {
+            leftCol = curShip.getBegPos().getCol();
+            rightCol = curShip.getEndPos().getCol();
+        }
+        for (int i = leftRow; i <= rightRow; i++) {
+            for (int j = leftCol; j <= rightCol; j++) {
+                field[i][j] = 'O';
+
+            }
+        }
+    }
+
+    // return true if too close to another ship
+    private boolean isTooCloseToAnotherShip(Ship curShip) {
+        int leftRow = 0;
+        int leftCol = 0;
+        int rightRow;
+        int rightCol;
+        if (curShip.getBegPos().getRow() >= curShip.getEndPos().getRow()) {
+            if (curShip.getBegPos().getRow() == SIZE_FIELD - 1)
+                rightRow = SIZE_FIELD - 1;
+            else
+                rightRow = curShip.getBegPos().getRow() + 1;
+            if (curShip.getEndPos().getRow() != 0)
+                leftRow = curShip.getEndPos().getRow() - 1;
+        } else {
+            if (curShip.getEndPos().getRow() == SIZE_FIELD - 1)
+                rightRow = SIZE_FIELD - 1;
+            else
+                rightRow = curShip.getEndPos().getRow() + 1;
+            if (curShip.getBegPos().getRow() != 0)
+                leftRow = curShip.getBegPos().getRow() - 1;
+        }
+        if (curShip.getBegPos().getCol() > curShip.getEndPos().getCol()) {
+            if (curShip.getBegPos().getCol() == SIZE_FIELD - 1)
+                rightCol = SIZE_FIELD - 1;
+            else
+                rightCol = curShip.getBegPos().getCol() + 1;
+            if (curShip.getEndPos().getCol() != 0)
+                leftCol = curShip.getEndPos().getCol() - 1;
+        } else {
+            if (curShip.getEndPos().getCol() == SIZE_FIELD - 1)
+                rightCol = SIZE_FIELD - 1;
+            else
+                rightCol = curShip.getEndPos().getCol() + 1;
+            if (curShip.getBegPos().getCol() == 0)
+                leftCol = 0;
+            else
+                leftCol = curShip.getBegPos().getCol() - 1;
+        }
+        for (int i = leftRow; i <= rightRow; i++) {
+            for (int j = leftCol; j <= rightCol; j++) {
+                if (field[i][j] != '~')
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private int convertToInt(String row) {
+        char symbol = row.charAt(0);
+        return (int) symbol - (int) START_LETTER;
+    }
+}

563527d5ec7234e24a56cbf4ba75ac34f5f5821c
Alexandru.Grubneac <grubneac@gmail.com>
2020-12-17 14:39:16 +0200

    Create function for calculate factorial and Unit test for it

---
+++ src/FactorialCalculatorTest.java
@@ -0,0 +1,44 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactorialCalculatorTest {
+
+    FactorialCalculator fc = new FactorialCalculator();
+
+    @Test
+    public void negativeTest() {
+        Assert.assertEquals(-1, fc.get(-1));
+    }
+
+    @Test
+    public void testGet0(){
+        Assert.assertEquals(1, fc.get(0));
+    }
+
+    @Test
+    public void testGet1(){
+        Assert.assertEquals(1, fc.get(1));
+    }
+
+    @Test
+    public void testGet2(){
+        Assert.assertEquals(2, fc.get(2));
+    }
+
+    @Test
+    public void testGet3(){
+        Assert.assertEquals(6, fc.get(3));
+    }
+
+    @Test
+    public void testGet4(){
+        Assert.assertEquals(24, fc.get(4));
+    }
+
+    @Test
+    public void testGet5(){
+        Assert.assertEquals(120, fc.get(5));
+    }
+}
